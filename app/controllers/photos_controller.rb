c3883967a26d187ea874bac57c98f67d294d64d4
SahilThakur7 <sahilrd07@gmail.com>
2017-07-25 18:39:00 +0530

    Image posting and showing done. Stars next

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,32 @@
 class PhotosController < ApplicationController
     before_action :set_user
     def index
+        @photo = Photo.new
+        @photos = Photo.order('created_at').where(user_id: @user.id)
 
+    end
+
+    def new
+        @photo = Photo.new
+    end
+
+    def create
+        @photo = Photo.new(photo_params)
+            @photo.set_user(@user)
+        if @photo.save
+            redirect_to user_photos_path(current_user)
+        else
+            flash[:notice] = "Your photo could not be saved"
+        end
     end
 
     private
 
     def set_user
         @user = User.find(params[:user_id])
+    end
+
+    def photo_params
+        params.require(:photo).permit(:pic,:caption)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :pic, styles: {medium: "300x300", thumbnail: "100x100>"}
+  validates_attachment_content_type :pic, :content_type => /\Aimage\/.*\Z/
+
+  def set_user(user)
+      self.user = user
+  end
 end
